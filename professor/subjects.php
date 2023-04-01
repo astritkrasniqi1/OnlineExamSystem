dfcafbb0cd34664777d7f190dcb446a5a93b5012
Alketa Zekaj <118227007+AlketaZekaj@users.noreply.github.com>
2023-04-01 19:07:40 +0200

    Online Exam System

--- professor/subjects.php
+++ professor/subjects.php
@@ -16,16 +16,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Subjects.css">
+    <link rel="stylesheet" href="subjects.css">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <style>
+    #inputForm {
+      display: none;
+    }
+  </style>
 </head>
 <body>
     <?php @include 'navbar.php' ?>
-</body>
-<section>
-<div class="pageTitleContainer" >   
+    <div class="pageTitleContainer" >   
                <div>
                     <h2>
                     <?php
@@ -35,62 +38,104 @@
                     ?>
                     </h2>
                </div>
-               <div>
-                    <a href="newSubject.php"><i class='bx bx-plus'></i>&nbsp;Add New Subject</a>
-               </div>
+               <button id="openFormButton"><i class='bx bx-plus'></i>&nbsp;Add New Subject</button>
+
+               <form id="inputForm">
+               <label for="name">Subject Name:</label>
+               <input type="text" id="name" name="name" required>
+               <br>
+   
+               <input type="submit" value="Add">
+                </form>
+               <script>
+                document.getElementById("openFormButton").addEventListener("click", function() {
+      document.getElementById("openFormButton").style.display = "none";
+      document.getElementById("inputForm").style.display = "block";
+    });
+  </script>
 
         </div>
 
-    <div class="subjectoverview" style="flex-wrap:wrap;">
-        <a href="#" class=""><div>
-        <i class="fa-solid fa-book" style="color:#f7b092;"></i>&nbsp;
-            <div class="col-auto"><h6>Computer Science</h6>
-              <span>Programming languages</span>
-        </div></div>
+        
+
+
+    <div class="filters">
+        <div><input type="search" placeholder="Search subject" /></div>
+        <div>
+            <select>
+                <option>All</option>
+            </select>
+            <button>
+                Filter&nbsp;<i class="bi bi-filter"></i>
+            </button>
+        </div>
+    </div>
+
+    <div class="subjectTable">
+    <table class="table ">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Subject Name</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row">1</td>
+      <td>Mathematic</td>
+      
+    </tr>
+    <tr>
+      <td scope="row">2</td>
+      <td>Physic</td>
+     
+
+    </tr>
+    <tr>
+      <td scope="row">3</td>
+      <td>English Language</td>
+     
+    </tr>
+  </tbody>
+</table>
+</div>
+
+<div class="subjectOverview" style="flex-wrap:wrap;">
+        <div class="topSubjects">
+            <div class="">
+                <h3>Top subjects</h3>
+            </div>
+        
+        </div>
+        <div class="subjectCards">
+        <a href="#" class="">
+        <div> <i class="fa-solid fa-book" style="color:#f7b092;"></i>&nbsp;
+            <div class="col-auto"><h6>Mathematic</h6></div></div>
         </a>
         
         <a href="#" class="">
-      <div>
-        <i class="fa-solid fa-book" style="color:#53b7ec;"></i>&nbsp;
-            <div class="col-auto"><h6>Mathematics</h6>
-                <span>Calculus, algebra, geometry</span>
-        </div>
-            
+      <div> <i class="fa-solid fa-book" style="color:#53b7ec;"></i>&nbsp;
+            <div class="col-auto"><h6>Physic</h6> </div>
         </div>
         </a>
-        <a href="#" class="">
-            <div>
-            <i class="fa-solid fa-book" style="color:#e96d7f;"></i>&nbsp;
-            <div class="col-auto"><h6>Sciences</h6>
-                <span>Biology, chemistry, physics</span></div>    
-            </div>
-        </a>
-        <a href="#" class="">
-            <div>
-            <i class="fa-solid fa-book" style="color:#93ccad;"></i>&nbsp;
-            <div class="col-auto"><h6>Humanities</h6>
-                <span>Philosophy,history,sociology</span></div>    
-            </div>
-        </a>
+        
          <a href="#" class="">
             <div>
             <i class="fa-solid fa-book" style="color:#73e40d;"></i>&nbsp;
-            <div class="col-auto"><h6>Languages</h6>
-                <span>English,German,French</span></div>    
+            <div class="col-auto"><h6>English Language</h6></div>    
             </div>
         </a>
-        <a href="#" class=""><div>
-        <i class="fa-solid fa-book" style="color:#b9b1e5;"></i>&nbsp;
-            <div class="col-auto"><h6>Medical</h6>
-                <span>Anatomy,physiology,pathology</span></div>
-            
-        </div>
-       
-       
+      
     </div>
-    
-    
 </section>
+
+
+
+
+</body>
+
+
 
 
 
