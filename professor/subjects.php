aef3fcd6b51d52b6ec6f979e7abebb12dd2d5047
Alketa Zekaj <118227007+AlketaZekaj@users.noreply.github.com>
2023-04-01 21:26:53 +0200

    Update subjects.php

--- professor/subjects.php
+++ professor/subjects.php
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="subject.css">
+    <link rel="stylesheet" href="Subject.css">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -48,7 +48,6 @@
                 </form>
                <script>
                 document.getElementById("openFormButton").addEventListener("click", function() {
-         document.getElementById("openFormButton").style.display = "none";
           document.getElementById("inputForm").style.display = "block";
           });
              </script>
