acd15acadd7c9bc5d50b649c93f1c99fb73494b6
Alketa Zekaj <118227007+AlketaZekaj@users.noreply.github.com>
2023-04-02 18:51:04 +0200

    .

--- professor/subjects.php
+++ professor/subjects.php
@@ -28,6 +28,85 @@
 </head>
 <body style="background:#f1f1f3;">
     <?php @include 'navbar.php' ?>
+
+<section>
+   <div class="pageTitleContainer" >   
+               <div>
+                    <h2>
+                    <?php
+                          $currentPage = basename($_SERVER['PHP_SELF']);
+                          $pageTitle = str_replace(".php", "", $currentPage);
+                          echo $pageTitle;  
+                    ?>
+                    </h2>
+               </div>
+               <button id="openFormButton"><i class='bx bx-plus'></i>&nbsp;Creat New Subject</button>
+                <form id="inputForm">
+               <label for="name">Subject Name:</label>
+               <input type="text" id="name" name="name" required>
+               <br>
+   
+               <input type="submit" value="Add">
+                </form>
+               <script>
+                document.getElementById("openFormButton").addEventListener("click", function() {
+                document.getElementById("inputForm").style.display = "block";});
+             </script>
+
+
+  </div>
+  
+ <div class="filters">
+        <div><input type="search" placeholder="Search subject" /></div>
+        <div>
+            <select>
+                <option>All</option>
+            </select>
+            <button>
+                Filter&nbsp;<i class="bi bi-filter"></i>
+            </button>
+        </div>
+    </div>
+
+    <div class="subjectTable">
+    <table class="table ">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Subject Name</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row">1</td>
+      <td>Mathematic</td>
+     
+    </tr>
+    <tr>
+      <td scope="row">2</td>
+      <td>Physic</td>
+    
+
+    </tr>
+    <tr>
+      <td scope="row">3</td>
+      <td>Chemistry</td>
+     
+    </tr>
+  </tbody>
+</table>
+</div>
+  
+
+  </section>
+
+
+
+
+
+
+
 </body>
 
 
@@ -43,7 +122,7 @@
             $('nav .logo-container ul li a.students').removeClass('active');
     })
     document.getElementById("openFormButton").addEventListener("click", function() {
-      document.getElementById("openFormButton").style.display = "none";
+  
       document.getElementById("inputForm").style.display = "block";
     });
 </script>
