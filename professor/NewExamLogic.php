5a4d7d65ce9b9d704d641eb5c984370ec94dcbe9
BlertOsmani <blertosmani15@gmail.com>
2023-04-03 19:53:58 +0200

    Online Exam System

--- professor/NewExamLogic.php
+++ professor/NewExamLogic.php
@@ -9,13 +9,13 @@
 
     if(isset($_POST['createExam'])){
         $examTitle = isset($_POST['exam-title']) ? $_POST['exam-title'] : '';
-        $subject = isset($_POST['subject']) ? (int)$_POST['subject'] : 0;
+        $subject = isset($_POST['subject']) ? $_POST['subject'] : '';
         $startDate = isset($_POST['startDate']) ? date('Y-m-d H:i:s', strtotime($_POST['startDate'])) : '';
         $duration = isset($_POST['duration']) ? $_POST['duration'] : '';
         $professorId = $_SESSION['professorID'];
 
         $sql = "INSERT INTO exam (Subject, Professor, Title, StartDate, Duration,Status, Created_at) 
-        VALUES ($subject, '$professorId', '$examTitle', '$startDate', '$duration', '' , NOW())";
+        VALUES ('$subject', '$professorId', '$examTitle', '$startDate', '$duration', '' , NOW())";
         if(mysqli_query($conn,$sql)){
             ?>
             <script>
